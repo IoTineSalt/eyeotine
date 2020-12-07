760b0569271b0968bfe2897a146a56ff416b9114
Jan Philipp Gampe <s6jagamp@uni-bonn.de>
2020-12-07 22:21:09 +0100

    First playing around with docker #3

---
+++ tests/docker/src/test.py
@@ -0,0 +1,12 @@
+import os
+i=0
+while(i<100):
+    hostname = "google.com" #example
+    response = os.system("ping -c 1 " + hostname)
+
+    #and then check the response...
+    if response == 0:
+      print(hostname, 'is up!')
+    else:
+      print( hostname, 'is down!')
+    i+=1
