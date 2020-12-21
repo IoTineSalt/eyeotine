cc10ad4c1f731b5524666759f783cfc2b83d5420
Joshua Gehlen <joshuagehlen@hotmail.com>
2020-12-21 21:53:30 +0100

    add ota mqtt tool #14

---
+++ src/ota/ota.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from paho.mqtt.client import Client
+import argparse
+
+parser = argparse.ArgumentParser(description='OTA-Updater')
+parser.add_argument('filename', type=str, help='filename to new binary')
+parser.add_argument('--host', '-H', type=str, default='localhost',
+                    help='host IP from the mqtt broker')
+parser.add_argument('--port', '-p', type=int, default=1883,
+                    help='host port')
+parser.add_argument('--topic', '-t', type=str, default='ota')
+args = parser.parse_args()
+
+client = Client()
+client.connect(args.host, args.port, 60)
+client.loop_start()
+with open(args.filename, 'rb') as file:
+    data = f.read()
+    return_value = client.publish(args.topic, data, qos=2)
+    if return_value[1] != 1:
+        exit(-1)
