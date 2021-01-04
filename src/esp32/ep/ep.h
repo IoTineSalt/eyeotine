b591cfe24b15e1ca71688861eafe0d3a5ff82ac3
Ben Swierzy <swierzy@uni-bonn.de>
2021-01-04 20:02:11 +0100

    Implemented most parts of ESP EP RX state machine #12

--- src/esp32/ep/ep.h
+++ src/esp32/ep/ep.h
@@ -5,19 +5,28 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
+
+// Possible log level:
+//   0 - No log
+//   1 - Errors
+//   2 - Warnings
+//   3 - Information
+//   4 - Debug
+#define EP_LOG_LEVEL 0
 
 struct ep_header {
     uint8_t ver_type_subtype;
     uint8_t payload[0];
 }; // todo Does the ESP compiler need __attribute__((packed)) here?
 
-#define EP_GET_VERSION(packet) ((packet->ver_type_subtype & 0xC0) >> 6)
-#define EP_GET_TYPE(packet) ((packet->ver_type_subtype & 0x30) >> 4)
-#define EP_GET_SUBTYPE(packet) (packet->ver_type_subtype & 0x0F)
+#define EP_GET_VERSION(packet) (((packet)->ver_type_subtype & 0xC0) >> 6)
+#define EP_GET_TYPE(packet) (((packet)->ver_type_subtype & 0x30) >> 4)
+#define EP_GET_SUBTYPE(packet) ((packet)->ver_type_subtype & 0x0F)
 
-#define EP_SET_VERSION(packet, val) packet->ver_type_subtype = (packet->ver_type_subtype & 0x3F) | ((val & 0x03) << 6)
-#define EP_SET_TYPE(packet, val) packet->ver_type_subtype = (packet->ver_type_subtype & 0xCF) | ((val & 0x03) << 4)
-#define EP_SET_SUBTYPE(packet, val) packet->ver_type_subtype = (packet->ver_type_subtype & 0xF0) | (val & 0x0F)
+#define EP_SET_VERSION(packet, val) (packet)->ver_type_subtype = ((packet)->ver_type_subtype & 0x3F) | ((val & 0x03) << 6)
+#define EP_SET_TYPE(packet, val) (packet)->ver_type_subtype = ((packet)->ver_type_subtype & 0xCF) | ((val & 0x03) << 4)
+#define EP_SET_SUBTYPE(packet, val) (packet)->ver_type_subtype = ((packet)->ver_type_subtype & 0xF0) | (val & 0x0F)
 
 #define EP_TYPE_MGMT 0
 #define EP_SUBTYPE_MGMT_ASSOC 0
@@ -65,11 +74,11 @@ struct ep_data_image {
     uint8_t data[0];
 };
 
-#define EP_GET_DATA_TIMESTAMP(packet) ((packet->timestamp_fragno & 0xFFC0) >> 6)
-#define EP_GET_DATA_FRAGMENT_NO(packet) (packet->timestamp_fragno & 0x003F)
+#define EP_GET_DATA_TIMESTAMP(packet) (((packet)->timestamp_fragno & 0xFFC0) >> 6)
+#define EP_GET_DATA_FRAGMENT_NO(packet) ((packet)->timestamp_fragno & 0x003F)
 
-#define EP_SET_DATA_TIMESTAMP(packet, val) packet->ver_type_subtype = (packet->ver_type_subtype & 0x003F) | ((val & 0x03FF) << 6)
-#define EP_SET_DATA_FRAGMENT_NO(packet, val) packet->ver_type_subtype = (packet->ver_type_subtype & 0xFFC0) | (val & 0x003F)
+#define EP_SET_DATA_TIMESTAMP(packet, val) (packet)->timestamp_fragno = ((packet)->timestamp_fragno & 0x003F) | ((val & 0x03FF) << 6)
+#define EP_SET_DATA_FRAGMENT_NO(packet, val) (packet)->timestamp_fragno = ((packet)->timestamp_fragno & 0xFFC0) | (val & 0x003F)
 
 // API Functions
 
@@ -77,9 +86,11 @@ enum ep_state {
     STOPPED, IDLE, ASSOCIATING, WAIT_FOR_SYNC, CAMERA_OFF, CAMERA_ON
 };
 
-void ep_init();
+void ep_init(int (*send_udp)(void *, size_t), uint16_t (*milliclk)());
 void ep_start();
 void ep_stop();
+void ep_recv_udp(void *buf, size_t len);
+int ep_send_img(void *buf, size_t len);
 void ep_set_connect_cb(void (*on_connect)());
 void ep_set_disconnect_cb(void (*on_disconnect)());
 void ep_set_recv_config_cb(void (*on_recv_config)(void *config, size_t len));
--- src/esp32/ep/ep_impl.c
+++ src/esp32/ep/ep_impl.c
@@ -7,6 +7,10 @@ struct ep {
     void *buffer;
     int sync;
 
+    // Interface
+    int (*send_udp)(void *, size_t);
+    uint16_t (*milliclk)();
+
     // Callbacks
     void (*on_connect)();
     void (*on_disconnect)();
@@ -14,15 +18,22 @@ struct ep {
     void (*on_recv_restart)();
 };
 
+// Internal declarations
 static struct ep ep;
+static bool recv(void *buf, size_t len);
+static inline bool handle_disassociation(struct ep_header *eph);
+static void perform_sync();
+static int send_img_frag(void *buf, size_t len, uint8_t frag_no, uint16_t timestamp);
 
-void ep_init() {
+void ep_init(int (*send_udp)(void *buf, size_t len), uint16_t (*milliclk)()) {
     ep = (struct ep) {
         .state = STOPPED,
         .time_last_tx = -1, // todo
         .time_last_rx = -1, // todo
         .sync = -1,
-        .buffer = (void *) 0xDEADBEEF // todo
+        .buffer = (void *) 0xDEADBEEF, // todo malloc 2300
+        .milliclk = milliclk,
+        .send_udp = send_udp,
     };
 }
 
@@ -33,8 +44,32 @@ void ep_start() {
 }
 
 void ep_stop() {
-    // todo handle termination
+    // todo handle termination, eg. send disconnect packet
     ep.state = STOPPED;
+}
+
+void ep_recv_udp(void *buf, size_t len) {
+    if (recv(buf, len)) {
+        ep.time_last_rx = ep.milliclk();
+    }
+}
+
+int ep_send_img(void *buf, size_t len) {
+    // todo error when state wrong
+    // Careful, when decrementing len, since it is unsigned (!)
+    size_t offset = 0;
+    uint8_t frag_no = 0;
+    uint16_t timestamp = (ep.milliclk() + 1024 - ep.sync) % 1024;
+    int err;
+    while (offset < len) {
+        size_t frag_size = (len - offset > 2200) ? 2200 : len - offset;
+        if ((err = send_img_frag(buf + offset, frag_size, frag_no, timestamp)) != 0) {
+            return err;
+        }
+        frag_no++;
+        offset += frag_size;
+    }
+    return 0;
 }
 
 void ep_set_connect_cb(void (*on_connect)()) {
@@ -53,82 +88,88 @@ void ep_set_recv_restart_cb(void (*on_recv_restart)()) {
     ep.on_recv_restart = on_recv_restart;
 }
 
-// Internal methods
-static void recv(void *buf, size_t len);
-static inline bool handle_disassociation(struct ep_header *eph);
-static void perform_sync();
-
 // Internal methods def
-static void recv(void *buf, size_t len) {
+static bool recv(void *buf, size_t len) {
     // Minimum length of the header is 1
     if (len < sizeof(struct ep_header)) {
-        return;
+        return false;
     }
 
     struct ep_header *eph = (struct ep_header *) buf;
     // Only version 0 allowed
     if (EP_GET_VERSION(eph) != 0) {
-        return;
+        return false;
     }
 
     // RX state machine
     if (ep.state == IDLE) {
-
         // Discard all packets
-        return;
-
+        return false;
     } else if (ep.state == ASSOCIATING) {
-
         if (EP_GET_TYPE(eph) != EP_TYPE_MGMT || EP_GET_SUBTYPE(eph) != EP_SUBTYPE_MGMT_ASSOC) {
             // Discard all packets except for association frames
-            return;
+            return false;
         }
 
-        if (len < sizeof(struct ep_mgmt_assoc))
-            return;
-
+        if (len < sizeof(struct ep_mgmt_assoc)) {
+            return false;
+        }
 
         struct ep_mgmt_assoc *assoc = (struct ep_mgmt_assoc *) buf;
         if ((assoc->flags & 1) == 0) {
             // Discard all requests
-            return;
+            return false;
         }
 
         // Association successful. Update state
+        if (ep.on_connect)
+            ep.on_connect();
         ep.state = WAIT_FOR_SYNC;
-        return;
+        return true;
+    } else if (ep.state == WAIT_FOR_SYNC || ep.state == CAMERA_OFF || ep.state == CAMERA_ON) {
+        if (handle_disassociation(eph)) return true;
 
-    } else if (ep.state == WAIT_FOR_SYNC) {
-
-        if (handle_disassociation(eph)) return;
-
-        if (EP_GET_TYPE(eph) != EP_TYPE_CTRL || EP_GET_SUBTYPE(eph) != EP_SUBTYPE_CTRL_SYNC) {
-            // Discard all packets except for association frames
-            return;
+        if (EP_GET_TYPE(eph) != EP_TYPE_CTRL || EP_GET_TYPE(eph) != EP_TYPE_MGMT) {
+            // Discard all packets except for mgmt and ctrl frames
+            return false;
         }
 
-        if (len < sizeof(struct ep_ctrl_sync))
-            return;
+        if (EP_GET_TYPE(eph) == EP_TYPE_CTRL && EP_GET_SUBTYPE(eph) == EP_SUBTYPE_CTRL_SYNC) {
+            if (len < sizeof(struct ep_ctrl_sync)) {
+                return false;
+            }
 
-        struct ep_ctrl_sync *sync = (struct ep_ctrl_sync *) buf;
-        perform_sync();
-        ep.state = CAMERA_OFF;
-        return;
+            perform_sync();
+            if (ep.state == WAIT_FOR_SYNC)
+                ep.state = CAMERA_OFF;
+            return true;
+        } else if (EP_GET_TYPE(eph) == EP_TYPE_CTRL && EP_GET_SUBTYPE(eph) == EP_SUBTYPE_CTRL_RESTART) {
+            if (len < sizeof(struct ep_ctrl_restart)) {
+                return false;
+            }
 
-    } else if (ep.state == CAMERA_OFF || ep.state == CAMERA_ON) {
-
-        if (handle_disassociation(eph)) return;
-
-        if (EP_GET_TYPE(eph) != EP_TYPE_CTRL && EP_GET_SUBTYPE(eph) != EP_SUBTYPE_CTRL_CONFIG) {
-            if (len < sizeof(struct ep_ctrl_config))
-                return;
+            if (ep.on_recv_restart)
+                ep.on_recv_restart();
+            return true;
+        } else if (EP_GET_TYPE(eph) == EP_TYPE_CTRL && EP_GET_SUBTYPE(eph) == EP_SUBTYPE_CTRL_CONFIG) {
+            if (len < sizeof(struct ep_ctrl_config)) {
+                return false;
+            }
 
             struct ep_ctrl_config *cfg = (struct ep_ctrl_config *) buf;
             if (ep.on_recv_config)
                 ep.on_recv_config(cfg->config, len - sizeof(struct ep_ctrl_config));
-            return;
-        }
+            return true;
+        } else if (EP_GET_TYPE(eph) == EP_TYPE_MGMT && EP_GET_SUBTYPE(eph) == EP_SUBTYPE_MGMT_PING) {
+            if (len < sizeof(struct ep_mgmt_ping))
+                return false;
 
+            // todo set the time
+            return true;
+        } else {
+            // Unknown packet type
+            return false;
+        }
     }
 }
 
@@ -144,5 +185,21 @@ static inline bool handle_disassociation(struct ep_header *eph) {
 }
 
 static void perform_sync() {
+    ep.sync = ep.milliclk() % 1024;
+}
 
+static int send_img_frag(void *buf, size_t len, uint8_t frag_no, uint16_t timestamp) {
+    struct ep_data_image *packet = (struct ep_data_image *) ep.buffer;
+    struct ep_header *header = (struct ep_header *) packet;
+
+    EP_SET_VERSION(header, 0);
+    EP_SET_TYPE(header, EP_TYPE_DATA);
+    EP_SET_SUBTYPE(header, EP_SUBTYPE_DATA_IMAGE);
+
+    EP_SET_DATA_TIMESTAMP(packet, timestamp);
+    EP_SET_DATA_FRAGMENT_NO(packet, frag_no);
+
+    memcpy(&(packet->data), buf, len);
+
+    return ep.send_udp(packet, sizeof(struct ep_data_image) + len);
 }
