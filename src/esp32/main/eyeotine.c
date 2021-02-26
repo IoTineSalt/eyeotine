f40c21ef718c5947f01217be0e6b5ac2e91cf6f5
Ben Swierzy <swierzy@uni-bonn.de>
2021-02-26 19:52:10 +0100

    Removed unnecessary components from the OTA example #15

--- src/esp32/main/eyeotine.c
+++ src/esp32/main/eyeotine.c
@@ -1,11 +1,3 @@
-/* OTA example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -15,23 +7,17 @@
 #include "esp_ota_ops.h"
 #include "esp_http_client.h"
 #include "esp_https_ota.h"
-#include "protocol_examples_common.h"
 #include "string.h"
 
 #include "nvs.h"
 #include "nvs_flash.h"
-#include "protocol_examples_common.h"
 #include "../ep/ep.h"
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 
-#if CONFIG_EXAMPLE_CONNECT_WIFI
-
 #include "esp_wifi.h"
-
-#endif
 
 static const char *TAGOTA = "OTA";
 static const char *TAG = "eyeotine";
@@ -71,30 +57,16 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
     return ESP_OK;
 }
 
-void simple_ota_example_task(void *pvParameter) {
+void ota_task(void *pvParameter) {
     ESP_LOGI(TAGOTA, "Starting OTA");
 
     esp_http_client_config_t config = {
-        .url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL,
+        .url = CONFIG_OTA_UPGRADE_URL,
         .cert_pem = (char *) server_cert_pem_start,
         .event_handler = _http_event_handler,
     };
 
-#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL_FROM_STDIN
-    char url_buf[OTA_URL_SIZE];
-    if (strcmp(config.url, "FROM_STDIN") == 0) {
-        example_configure_stdin_stdout();
-        fgets(url_buf, OTA_URL_SIZE, stdin);
-        int len = strlen(url_buf);
-        url_buf[len - 1] = '\0';
-        config.url = url_buf;
-    } else {
-        ESP_LOGE(TAGOTA, "Configuration mismatch: wrong firmware upgrade image url");
-        abort();
-    }
-#endif
-
-#ifdef CONFIG_EXAMPLE_SKIP_COMMON_NAME_CHECK
+#ifdef CONFIG_OTA_SKIP_COMMON_NAME_CHECK
     config.skip_cert_common_name_check = true;
 #endif
 
@@ -111,18 +83,12 @@ void simple_ota_example_task(void *pvParameter) {
     }
 }
 
+static struct sockaddr_in sin = {
+    .sin_family = AF_INET,
+    .sin_port = PP_HTONS(CONFIG_EYEOTINE_SERVER_PORT),
+};
+
 int eyeotine_send_udp(void *buf, size_t len) {
-    // todo create config for this
-    const char *ip = "192.168.0.2";
-    uint16_t port = 2021;
-
-    // todo don't calculate this repeatedly
-    struct sockaddr_in sin = {
-        .sin_port = htons(port),
-        .sin_family = AF_INET,
-        .sin_addr.s_addr = inet_addr(ip)
-    };
-
     // todo retry a few times if ERRWOULDBLOCK
     int err = sendto(udpsock, buf, len, 0, (struct sockaddr *) &sin, sizeof(sin));
 
@@ -170,6 +136,8 @@ void eyeotine_task(void *pvParameter) {
 
     int flags = fcntl(udpsock, F_GETFL);
     fcntl(udpsock, F_SETFL, flags | O_NONBLOCK);
+
+    sin.sin_addr.s_addr = inet_addr(CONFIG_EYEOTINE_SERVER_IP);
 
     ESP_LOGI(TAG, "Created UDP socket");
 
@@ -256,7 +224,7 @@ static camera_config_t camera_config = {
 };
 
 void eyeotine_camera_task(void *pvParameter) {
-    // Camera init
+//    // Camera init (not sure if needed)
 //    if (CAM_PIN_PWDN != -1) {
 //        pinMode(CAM_PIN_PWDN, OUTPUT);
 //        digitalWrite(CAM_PIN_PWDN, LOW);
@@ -280,6 +248,137 @@ void eyeotine_camera_task(void *pvParameter) {
 
 }
 
+// The next methods are copied from esp-idf's protocol_examples_common.
+// Maybe we can improve them since they might be designed for a broader use case than ours.
+
+static xSemaphoreHandle s_semph_get_ip_addr;
+static esp_ip4_addr_t s_ip_addr;
+static esp_netif_t *s_example_esp_netif;
+
+/**
+ * @brief Checks the netif description if it contains specified prefix.
+ * All netifs created withing common connect component are prefixed with the module TAG,
+ * so it returns true if the specified netif is owned by this module
+ */
+static bool is_our_netif(const char *prefix, esp_netif_t *netif)
+{
+    return strncmp(prefix, esp_netif_get_desc(netif), strlen(prefix) - 1) == 0;
+}
+
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    esp_err_t err = esp_wifi_connect();
+    if (err == ESP_ERR_WIFI_NOT_STARTED) {
+        return;
+    }
+    ESP_ERROR_CHECK(err);
+}
+
+static void on_got_ip(void *arg, esp_event_base_t event_base,
+                      int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    if (!is_our_netif(TAG, event->esp_netif)) {
+        ESP_LOGW(TAG, "Got IPv4 from another interface \"%s\": ignored", esp_netif_get_desc(event->esp_netif));
+        return;
+    }
+    ESP_LOGI(TAG, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
+    memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+    xSemaphoreGive(s_semph_get_ip_addr);
+}
+
+static esp_netif_t *wifi_start(void)
+{
+    char *desc;
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+    // Prefix the interface description with the module TAG
+    // Warning: the interface desc is used in tests to capture actual connection details (IP, gw, mask)
+    asprintf(&desc, "%s: %s", TAG, esp_netif_config.if_desc);
+    esp_netif_config.if_desc = desc;
+    esp_netif_config.route_prio = 128;
+    esp_netif_t *netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+    free(desc);
+    esp_wifi_set_default_wifi_sta_handlers();
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
+
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_EYEOTINE_WIFI_SSID,
+            .password = CONFIG_EYEOTINE_WIFI_PASSWORD,
+        },
+    };
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    return netif;
+}
+
+esp_netif_t *get_example_netif_from_desc(const char *desc)
+{
+    esp_netif_t *netif = NULL;
+    char *expected_desc;
+    asprintf(&expected_desc, "%s: %s", TAG, desc);
+    while ((netif = esp_netif_next(netif)) != NULL) {
+        if (strcmp(esp_netif_get_desc(netif), expected_desc) == 0) {
+            free(expected_desc);
+            return netif;
+        }
+    }
+    free(expected_desc);
+    return netif;
+}
+
+static void wifi_stop(void)
+{
+    esp_netif_t *wifi_netif = get_example_netif_from_desc("sta");
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
+    esp_err_t err = esp_wifi_stop();
+    if (err == ESP_ERR_WIFI_NOT_INIT) {
+        return;
+    }
+    ESP_ERROR_CHECK(err);
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+    ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(wifi_netif));
+    esp_netif_destroy(wifi_netif);
+    s_example_esp_netif = NULL;
+}
+
+esp_err_t wifi_connect(void)
+{
+    if (s_semph_get_ip_addr != NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+    s_example_esp_netif = wifi_start();
+    s_semph_get_ip_addr = xSemaphoreCreateCounting(1, 0);
+    ESP_ERROR_CHECK(esp_register_shutdown_handler(&wifi_stop));
+    ESP_LOGI(TAG, "Waiting for IP(s)");
+    xSemaphoreTake(s_semph_get_ip_addr, portMAX_DELAY);
+    // iterate over active interfaces, and print out IPs of "our" netifs
+    esp_netif_t *netif = NULL;
+    esp_netif_ip_info_t ip;
+    for (int i = 0; i < esp_netif_get_nr_of_ifs(); ++i) {
+        netif = esp_netif_next(netif);
+        if (is_our_netif(TAG, netif)) {
+            ESP_LOGI(TAG, "Connected to %s", esp_netif_get_desc(netif));
+            ESP_ERROR_CHECK(esp_netif_get_ip_info(netif, &ip));
+
+            ESP_LOGI(TAG, "- IPv4 address: " IPSTR, IP2STR(&ip.ip));
+        }
+    }
+    return ESP_OK;
+}
+
 void app_main(void) {
     // Initialize NVS.
     esp_err_t err = nvs_flash_init();
@@ -295,21 +394,11 @@ void app_main(void) {
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(wifi_connect());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
-
-#if CONFIG_EXAMPLE_CONNECT_WIFI
-    /* Ensure to disable any WiFi power save mode, this allows best throughput
-     * and hence timings for overall OTA operation.
-     */
     esp_wifi_set_ps(WIFI_PS_NONE);
-#endif // CONFIG_EXAMPLE_CONNECT_WIFI
 
-    xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&ota_task, "ota_task", 8192, NULL, 5, NULL);
     xTaskCreate(&eyeotine_task, "eyeotine_task", 8192, NULL, 5, NULL);
     xTaskCreate(&eyeotine_camera_task, "eyeotine_camera_task", 8192, NULL, 4, NULL);
 }
